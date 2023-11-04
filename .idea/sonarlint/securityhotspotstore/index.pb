96a622a7dfae6b572515310fe9a424d2ea966f1a
Василий Мешков <vaskyinki@yandex.ru>
2023-11-04 23:55:50 +0300

    V2

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+n
+>src/main/java/com/mvas/server/controller/ServerController.java,2\4\24abc2ced988362a76ef696ef8e6646211fd9575
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2\c\2cabb939da04d3de3a200bf2b575690e1a16316d
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
+Å
+Q.idea/sonarlint/securityhotspotstore/2/4/24abc2ced988362a76ef696ef8e6646211fd9575,8\b\8b1dfbc7eec56795c40a6d7a9abd1c2273a392e0
--- src/main/java/com/mvas/server/controller/ServerController.java
+++ src/main/java/com/mvas/server/controller/ServerController.java
@@ -1,17 +1,71 @@
 package com.mvas.server.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import javax.crypto.Cipher;
+
 @RestController
 public class ServerController {
-    @ResponseBody
-    @PostMapping("/receiveMessage")
-    public String receiveMessage(@RequestParam("message") String message) {
 
-        return message;
+    private RSAPrivateKey serverPrivateKey;
+    private RSAPublicKey serverPublicKey;
+
+    public ServerController() throws NoSuchAlgorithmException {
+        KeyPair keyPair = generateRSAKeyPair();
+        serverPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+        serverPublicKey = (RSAPublicKey) keyPair.getPublic();
+    }
+
+    @PostMapping("/receivePublicKey")
+    public String receivePublicKey(@RequestBody String publicKeyBase64) {
+        try {
+            return Base64.getEncoder().encodeToString(serverPublicKey.getEncoded());
+
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/getServerPublicKey")
+    public String getServerPublicKey() {
+        return Base64.getEncoder().encodeToString(serverPublicKey.getEncoded());
+    }
+
+    @PostMapping("/receiveMessage")
+    public String receiveMessage(@RequestBody String encryptedMessage) {
+        try {
+            String decryptedMessage = decryptWithServerPrivateKey(encryptedMessage);
+
+            String response = "Accept: " + decryptedMessage;
+            return response;
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    private KeyPair generateRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    private String decryptWithServerPrivateKey(String encryptedMessage) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, serverPrivateKey);
+
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMessage);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes, "UTF-8"); // Указываем кодировку UTF-8
     }
 }
-
