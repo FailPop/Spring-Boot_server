180075574806028882229b9d5e038084f5cfb8bb
Василий Мешков <vaskyinki@yandex.ru>
2023-11-15 05:15:52 +0300

    Project done

--- src/main/java/com/mvas/server/controller/ServerController.java
+++ src/main/java/com/mvas/server/controller/ServerController.java
@@ -7,18 +7,17 @@ import com.puppycrawl.tools.checkstyle.api.AuditEvent;
 import com.puppycrawl.tools.checkstyle.api.AuditListener;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,71 +27,71 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
 @RestController
 public class ServerController {
-
     private static final Logger logger = LogManager.getLogger(ServerController.class);
-
     private RSAPrivateKey serverPrivateKey;
     private RSAPublicKey serverPublicKey;
-
     private RSAPublicKey clientPublicKey;
 
     @PostMapping("/receiveClientPublicKey")
     public void receiveClientPublicKey(@RequestBody String publicKey) throws Exception {
+        logger.info("Received client public key: " + publicKey);
         this.clientPublicKey = decodeRSAPublicKey(publicKey);
     }
 
     public ServerController() throws NoSuchAlgorithmException {
-        logger.info("GenerateRSAKeyPair");
+        logger.info("Generate RSA key Pair");
         KeyPair keyPair = generateRSAKeyPair();
         serverPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
         serverPublicKey = (RSAPublicKey) keyPair.getPublic();
+        logger.info("Generate RSA key Pair done");
     }
 
     @PostMapping("/getServerPublicKey")
     public String getServerPublicKey() {
-        logger.info("GetServerPublicKey");
+        logger.info("Get Server Public Key");
         return Base64.getEncoder().encodeToString(serverPublicKey.getEncoded());
     }
 
     @PostMapping("/receiveEncryptedData")
     public String receiveEncryptedData(@RequestBody String encryptedData) {
         try {
-            logger.info("ReceiveEncryptedData");
+            logger.info("Receive Encrypted Data: " + encryptedData);
             Security.addProvider(new BouncyCastleProvider());
             String[] parts = encryptedData.split(":");
-
+            logger.info("Cipher --> ");
             Cipher rsaCipher = Cipher.getInstance("RSA/None/OAEPWithSHA1AndMGF1Padding", "BC");
             rsaCipher.init(Cipher.DECRYPT_MODE, serverPrivateKey);
             byte[] encryptedAesKey = Base64.getDecoder().decode(parts[0]);
             byte[] decryptedAesKey = rsaCipher.doFinal(encryptedAesKey);
-
+            logger.info("Decrypted AES key: " + Arrays.toString(decryptedAesKey));
             SecretKeySpec aesKey = new SecretKeySpec(decryptedAesKey, "AES");
             Cipher aesCipher = Cipher.getInstance("AES");
             aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
             byte[] encryptedMessage = Base64.getDecoder().decode(parts[1]);
             byte[] decryptedMessage = aesCipher.doFinal(encryptedMessage);
-
+            logger.info("Decrypted Message: " + new String(decryptedMessage, StandardCharsets.UTF_8));
             Path file = Paths.get("src/main/resources/clientcode.java");
             Files.write(file, decryptedMessage);
-
             // Анализ файла с использованием Checkstyle
             String errors = analyzeCheckstyle(file);
-
             // Шифрование ошибок с помощью AES и RSA
             String encryptedErrors = encryptWithClientPublicKey(errors);
-
             return encryptedErrors;
-
         } catch (Exception e) {
             return "Unexpected error, please write your code";
         }
     }
+
+
     private String encryptWithClientPublicKey(String message) throws Exception {
+        logger.info("Encrypt With Client PublicKey");
+
         Security.addProvider(new BouncyCastleProvider());
 
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
@@ -101,7 +100,7 @@ public class ServerController {
         Cipher aesCipher = Cipher.getInstance("AES");
         aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
         byte[] encryptedMessage = aesCipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
-
+        logger.info("Encrypted Message: " + encryptedMessage);
         Cipher rsaCipher = Cipher.getInstance("RSA/None/OAEPWithSHA1AndMGF1Padding", "BC");
         rsaCipher.init(Cipher.ENCRYPT_MODE, clientPublicKey);
         byte[] encryptedAesKey = rsaCipher.doFinal(aesKey.getEncoded());
@@ -109,7 +108,8 @@ public class ServerController {
         return Base64.getEncoder().encodeToString(encryptedAesKey) + ":" + Base64.getEncoder().encodeToString(encryptedMessage);
     }
 
-    private String analyzeCheckstyle(Path filePath) throws IOException, CheckstyleException {
+    private String analyzeCheckstyle(Path filePath) throws CheckstyleException {
+        logger.info("Analyze code");
         // Загрузка конфигурации Checkstyle из ресурсов
         Configuration config = ConfigurationLoader.loadConfiguration("src/main/resources/checkstyle-config.xml", new PropertiesExpander(System.getProperties()));
 
@@ -141,12 +141,16 @@ public class ServerController {
 
             @Override
             public void addError(AuditEvent event) {
-                errorMessages.add("Error: " + event.getMessage() + ", line: " + event.getViolation().getLineNo() + ", column: " + event.getViolation().getColumnNo());
+                String errorMessage = "Error: " + event.getMessage() + ", line: " + event.getViolation().getLineNo() + ", column: " + event.getViolation().getColumnNo();
+                errorMessages.add(errorMessage);
+
             }
 
             @Override
             public void addException(AuditEvent event, Throwable throwable) {
-                errorMessages.add("Exception: " + event.getMessage());
+                String errorMessage = "Exception: " + event.getMessage();
+                errorMessages.add(errorMessage);
+
             }
         };
 
@@ -162,18 +166,19 @@ public class ServerController {
             errorList.append(errorMessage).append("\n");
         }
 
+        logger.info("Error list done");
         return errorList.toString();
     }
 
-
     private RSAPublicKey decodeRSAPublicKey(String publicKeyBase64) throws Exception {
+        logger.info("Decode RSA Public Key");
         byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyBase64);
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
         return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(keySpec);
     }
 
     private KeyPair generateRSAKeyPair() throws NoSuchAlgorithmException {
-        logger.info("GenerateRSAKeyPair");
+        logger.info("Generate RSA Key Pair");
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(2048);
         return keyGen.generateKeyPair();
